18e35dab784c6d2be220870c1163c72c82d43a77
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-06-09 20:05:40 -0500

    add product from API

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,11 +1,23 @@
+import { useState, useEffect } from "react"
 import Layout from "../../Components/Layout"
 import Card from "../../Components/Card"
 function Home() {
+  const [items, setItems] = useState(null)
+
+  useEffect(() => {
+    fetch("https://api.escuelajs.co/api/v1/products")
+      .then((res) => res.json())
+      .then((data) => setItems(data))
+  }, [])
 
   return (
     <Layout >
        Home
-      <Card />
+       <div className="grid grid-cols-4 gap-4 w-full max-w-screen-lg">
+       {
+        items?.map((item) => <Card key={item.id} data={item} />)
+       }
+       </div>
     </Layout>
     
   )
