fe05a9be106b3e008c50ebdc0b46a1281702f2c1
wlaurentec <152199005+wlaurentec@users.noreply.github.com>
2024-06-12 15:52:56 -0500

    to view totalProducts={order.total}

--- src/Components/OrdersCard/index.jsx
+++ src/Components/OrdersCard/index.jsx
@@ -1,15 +1,15 @@
-import { XMarkIcon } from "@heroicons/react/24/solid";
-
 const OrdersCard = (props) => {
-  const { toalPrice, totalProducts } = props;
+  const { totalPrice, totalProducts } = props;
 
   return (
-    <div className="flex justify-between items-center mb-3 border border-black">
-      <p>
-        <span>01.02.2023</span>
-        <span>{totalProducts}</span>
-        <span>${toalPrice}</span>
-      </p>
+    <div className="flex justify-between items-center mb-3 border border-black p-4 w-80 rounded-lg">
+      <div className="flex justify-between w-full">
+        <div className="flex flex-col">
+          <span className="font-light">01.02.2023</span>
+          <span className="font-light">{totalProducts} articles</span>
+        </div>
+        <span className="font-medium text-2xl">${totalPrice}</span>
+      </div>
     </div>
   );
 };
--- src/Pages/MyOrders/index.jsx
+++ src/Pages/MyOrders/index.jsx
@@ -9,16 +9,17 @@ function MyOrders() {
 
   return (
     <Layout>
-      <div className="flex items-center justify-center relative w-80 ">
-        <h1>My Orders</h1>
+      <div className="flex items-center justify-center relative w-80 mb-4 ">
+        <h1 className="font-medium text-xl">My Orders</h1>
       </div>
 
       {context.order.map((order, index) => {
         return (
           <Link key={index} to={`/my-orders/${index}`}>
             <OrdersCard
-              toalPrice={order.totalPrice}
-              totalProducts={order.totalProducts}
+              totalPrice={order.totalPrice}
+              totalProducts={order.total}
+              
             />
           </Link>
         );
